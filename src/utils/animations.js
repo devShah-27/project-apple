70eba32e63400d1fe35c355a32f0ac22d2e618a6
Dev Shah <shahdev149@gmail.com>
2024-07-18 08:45:33 +0530

    create features section

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,17 +1,49 @@
-export const animateWithGsapTimeline=(timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps)=>{
-    timeline.to(rotationRef.current.rotation,{
-        y:rotationState,
-        duration:1,
-        ease:'power2.inOut'
-    })
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
-    timeline.to(firstTarget,{
-        ...animationProps,
-        ease:'power2.inOut'
-    }, '<')
+gsap.registerPlugin(ScrollTrigger)
 
-    timeline.to(secondTarget,{
-        ...animationProps,
-        ease:'power2.inOut'
-    }, '<')
-}
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start:'top 85%',
+      ...scrollProps
+    },
+  });
+};
+
+export const animateWithGsapTimeline = (
+  timeline,
+  rotationRef,
+  rotationState,
+  firstTarget,
+  secondTarget,
+  animationProps
+) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+};
